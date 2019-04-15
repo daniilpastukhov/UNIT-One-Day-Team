ae1b8aaa53a4b1c6f534da49b7cbcb5bded36f0b
sokovnik <sokovnik@fel.cvut.cz>
2019-04-15 10:02:38 +0200

    DetectedObjectManager function added

--- android/app/src/main/java/org/tensorflow/lite/examples/detection/ecoapp/DetectedObjectsManager.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/ecoapp/DetectedObjectsManager.java
@@ -1,4 +1,8 @@
 package org.tensorflow.lite.examples.detection.ecoapp;
 
 public class DetectedObjectsManager {
+
+    public void drawHint(int[] coordinates) {
+        //TODO Handle all Managers
+    }
 }
