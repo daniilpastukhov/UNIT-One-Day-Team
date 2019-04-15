60929db0c1511d6a5aa1df54e2da830b20035d8c
Barinov <barinale@fel.cvut.cz>
2019-04-15 10:36:12 +0200

    have added some constants

--- android/app/src/main/java/org/tensorflow/lite/examples/detection/ecoapp/Constants.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/ecoapp/Constants.java
@@ -3,4 +3,7 @@ package org.tensorflow.lite.examples.detection.ecoapp;
 public class Constants {
     public static int SCREENWIDTH;
     public static int SCREENHEIGHT;
+
+    public static int textBoxWidth = 200;
+    public static int textBoxWHeight = 400;
 }
--- android/app/src/main/java/org/tensorflow/lite/examples/detection/ecoapp/CoordinatesManager.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/ecoapp/CoordinatesManager.java
@@ -1,13 +1,15 @@
 package org.tensorflow.lite.examples.detection.ecoapp;
 
+import android.graphics.Rect;
+
 public class CoordinatesManager {
     /*to do
-    get detected box coordinates int[] = {x,y,width,height}
+    get detected box coordinates Rect (x,y,width,height)
     compute text box position
-    return int[] = {x,y,width,height}
+    return Rect  (x,y,width,height)
      */
-    public static int[] getTextBoxPosition() {
-        int[] boxParams = new int[4];
+    public static Rect getTextBoxPosition(Rect detectedBox) {
+        Rect boxParams = new Rect();
 
         return boxParams;
     }
