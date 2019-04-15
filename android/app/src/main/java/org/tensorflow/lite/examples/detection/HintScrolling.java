c6f5b0c8ce50a5fb42cf395d1da94e4deb96c096
Daniil Pastukhov <pastudan@fit.cvut.cz>
2019-04-15 15:41:27 +0200

    Camera fix

--- android/app/src/main/java/org/tensorflow/lite/examples/detection/HintScrolling.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/HintScrolling.java
@@ -1,15 +1,21 @@
 package org.tensorflow.lite.examples.detection;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import org.tensorflow.lite.examples.detection.ecoapp.HintManager;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class HintScrolling extends AppCompatActivity {
@@ -31,27 +37,28 @@ public class HintScrolling extends AppCompatActivity {
         });
     }
 
-    class Hints {
-        LinkedList<String> objs;
+    private LinkedList<String> getKeys() {
+        LinkedList<String> objs = new LinkedList<String>();
+        for (String key : HintManager.hints.keySet()) {
+            objs.push(key);
+        }
 
-        public Hints() {
-            HintManager hm = new HintManager();
-            for (String key: hm.hints.keySet()) {
-                objs.push(key);
-            }
+        return objs;
+    }
+
+    private void addHints() {
+        LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = vi.inflate(R.layout.activity_hint_scrolling, null);
+        LinkedList<String> hintsList = getKeys();
+
+        for (String hint: hintsList) {
+            TextView textView = v.findViewById(R.id.hint_view);
+            textView.setText(hint);
+
+            ViewGroup insertPoint = findViewById(R.id.scrollingView);
+            insertPoint.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
         }
     }
 
-    class HintList {
-        private Activity context;
-        private LinearLayout hintList;
 
-
-
-        public HintList(Activity context, String[] nameArrayParam, String[] infoArrayParam) {
-
-        }
-
-
-    }
 }
