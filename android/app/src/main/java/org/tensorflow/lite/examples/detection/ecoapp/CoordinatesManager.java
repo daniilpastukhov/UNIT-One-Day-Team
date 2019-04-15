c4d5e54e78f44f1d33c799939ec11c595c2345b2
Barinov <barinale@fel.cvut.cz>
2019-04-15 10:10:54 +0200

    method declaration

--- android/app/src/main/java/org/tensorflow/lite/examples/detection/ecoapp/CoordinatesManager.java
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/ecoapp/CoordinatesManager.java
@@ -2,8 +2,13 @@ package org.tensorflow.lite.examples.detection.ecoapp;
 
 public class CoordinatesManager {
     /*to do
-    get detected box coordinates
+    get detected box coordinates int[] = {x,y,width,height}
     compute text box position
     return int[] = {x,y,width,height}
      */
+    public static int[] getTextBoxPosition() {
+        int[] boxParams = new int[4];
+
+        return boxParams;
+    }
 }
